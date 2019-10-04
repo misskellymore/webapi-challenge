91766acc2a7b5ba9d4c1049f882cddfeafc06753
Kelly Moreira <moreira.kd@outlook.com>
2019-10-04 13:16:55 -0400

    insert update and remove on projects

--- routes/project.js
+++ routes/project.js
@@ -33,6 +33,59 @@ router.get('/:id', validateProjectId, (req, res) => {
 })
 
 
+// Post /projects
+router.post('/', validateProject, (req, res) => {
+    const project = req.project;
+
+    Projects.insert(project)
+    .then(added => {
+        res.status(201).json(added);
+    })
+    .catch(err => {
+        res.status(500).json({ err: 'error adding project' })
+    })
+
+})
+
+
+// Delete /projects/id
+
+router.delete('/:id', validateProjectId, (req, res) => {
+    const project = req.project;
+
+    Projects.remove(project.id)
+    .then(deleted => {
+        res.status(200).json(deleted);
+    })
+    .catch(err => {
+        res.status(500).json({ err: 'error deleting' });
+    })
+})
+
+
+// Put /projects/id
+
+router.put('/:id', validateProjectId, (req, res) => {
+    const project = req.project;
+    const changes = req.body;
+
+    if(!changes.name && !changes.description) {
+        res.status(400).json({ message: 'Update name or description' })
+    } else {
+
+        Projects.update(project.id, changes)
+        .then(updated => {
+            res.status(200).json(updated);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'error updating' });
+        })
+    }
+
+
+
+})
+
 // middleware
 
 function validateProjectId(req, res, next) {
@@ -49,6 +102,18 @@ function validateProjectId(req, res, next) {
     .catch(err => {
         res.status(500).json({ message: 'error accessing project data' });
     })
+}
+
+
+function validateProject(req, res, next) {
+    const projectBody = req.body;
+
+    if(!projectBody.name || !projectBody.description) {
+        res.status(400).json({ message: 'missing fields' })
+    } else {
+        req.project = projectBody;
+        next();
+    }
 }
 
 
