bdfae69c501b5f884d0b60c0ba086f182e1bb329
Kelly Moreira <moreira.kd@outlook.com>
2019-10-04 12:28:47 -0400

    some gets on project

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const Projects = require('../data/helpers/projectModel.js');
+
 const router = express.Router();
 
-const Projects = require('../data/helpers/projectModel.js');
+
+// Get /projects
 
 router.get('/', (req, res) => {
     Projects.get()
@@ -13,6 +16,40 @@ router.get('/', (req, res) => {
         res.status(500).json({ err: 'error getting projects' })
     })
 })
+
+
+// Get /projects/id
+
+router.get('/:id', validateProjectId, (req, res) => {
+    const project = req.project;
+
+    Projects.get(project.id)
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(err => {
+        res.status(500).json({ err: 'error getting project data' })
+    })
+})
+
+
+// middleware
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+
+    Projects.get(id)
+    .then(project => {
+        if (project) {
+            req.project = project;
+            next();
+        } else {
+            res.status(404).json({ message: 'no project with this id' });        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'error accessing project data' });
+    })
+}
 
 
 module.exports = router;
